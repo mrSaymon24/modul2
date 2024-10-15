3d8ae79d8dca2e056bee2faff63c06bbaf5df944
mrSaymon24 <pisyaka95@gmail.com>
2024-10-15 21:07:59 +0700

    Add files via upload

---
+++ modul2.5.py
@@ -0,0 +1,18 @@
+from zoneinfo import reset_tzpath
+
+
+def get_matrix(n, m, volue):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(volue)
+        matrix.append(row)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
